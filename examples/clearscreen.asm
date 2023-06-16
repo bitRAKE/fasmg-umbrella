374df22a936f9109bf9a4e44f128f43514234e29
Rickey Bowers Jr <bitRAKE@gmail.com>
2023-06-16 11:36:24 -0600

    update clear screen example to support windows 10

--- examples/clearscreen.asm
+++ examples/clearscreen.asm
@@ -23,8 +23,10 @@ main:
 		.P5	dq ?	; fifth parameter
 
 	; some local space on the stack for temporary data:
+		.hStdIn		dq ?
 		.hStdOut	dq ?
 		.dwTemp		dd ?
+		.mode		dd ?
 
 		align.assume rsp, 16
 		align 16 | 8
@@ -33,9 +35,41 @@ main:
 	end virtual
 	sub rsp, .stack_offset
 
+	mov rcx, -10 ; STD_INPUT_HANDLE
+	call [GetStdHandle]
+	mov [.hStdIn], rax
+
 	mov rcx, -11 ; STD_OUTPUT_HANDLE
 	call [GetStdHandle]
 	mov [.hStdOut], rax
+
+; https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
+
+	lea rdx, [.mode]
+	mov rcx, [.hStdIn]
+	call [GetConsoleMode]
+	xchg ecx, eax ; BOOL
+	jrcxz .fail
+
+	mov edx, [.mode]
+	or edx, 0x0200 ; ENABLE_VIRTUAL_TERMINAL_INPUT
+	mov rcx, [.hStdIn]
+	call [SetConsoleMode]
+	xchg ecx, eax ; BOOL
+	jrcxz .fail
+
+	lea rdx, [.mode]
+	mov rcx, [.hStdOut]
+	call [GetConsoleMode]
+	xchg ecx, eax ; BOOL
+	jrcxz .fail
+
+	mov edx, [.mode]
+	or edx, 0x000C ; ENABLE_VIRTUAL_TERMINAL_PROCESSING or DISABLE_NEWLINE_AUTO_RETURN
+	mov rcx, [.hStdOut]
+	call [SetConsoleMode]
+	xchg ecx, eax ; BOOL
+	jrcxz .fail
 
 	mov [.P5], 0
 	lea r9, [.dwTemp]
@@ -43,7 +77,7 @@ main:
 	lea rdx, [_clear_screen]
 	mov rcx, [.hStdOut]
 	call [WriteConsoleA]
-
+.fail:
 	mov ecx, 0
 	call [ExitProcess]
 	int3
@@ -54,5 +88,7 @@ library kernel32, 'kernel32.dll'
 
 import kernel32,\
 	ExitProcess, 'ExitProcess',\
+	GetConsoleMode, 'GetConsoleMode',\
 	GetStdHandle, 'GetStdHandle',\
+	SetConsoleMode, 'SetConsoleMode',\
 	WriteConsoleA, 'WriteConsoleA'
